f4287506d2bc295d6045f4576a1eef9672aeacee
Maho Pacheco <mictlan@gmail.com>
2024-03-18 21:59:31 +0000

    Updating scripts

--- scripts/02-deploy.sh
+++ scripts/02-deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 hugo deploy --maxDeletes 0
-Rss2Outbox --rssPath public/index.xml \
-    --staticPath static \
-    --authorUsername "\@mapache@hachyderm.io" \
-    --siteActorUri "https://maho.dev/@blog" \
-    --domain "https://maho.dev"
-hugo
