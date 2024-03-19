4ff2b08cdb1ca723d8beea0cf8c9551a4772efa2
Maho Pacheco <mictlan@gmail.com>
2024-03-19 23:39:37 +0000

    Updating deploy

--- utils/12-deploy.sh
+++ utils/12-deploy.sh
@@ -7,24 +7,19 @@ prefix=$(cd $script_dir/../blog/public/ && pwd)
 
 hugo deploy --maxDeletes 0 --logLevel debug
 
-for file in $socialweb_dir/*; do
-    filePath="${file#$prefix}"
-    filePath="${filePath#?}"
+# Loop through files in socialweb_dir
+find "$socialweb_dir" -type f -exec bash -c '
+    prefix="$1"
+    shift
+    for file; do
+        filePath="${file#$prefix}"
+        filePath="${filePath#?}"
 
-    echo $filePath
-    
-    # Use Azure CLI to set content type
-    az storage blob update -c "\$web" -n "$filePath" --content-type "application/activity+json;" --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT"
-done
+        echo "$filePath"
 
-for file in $socialweb_dir/notes/*; do
-    filePath="${file#$prefix}"
-    filePath="${filePath#?}"
+        # Use Azure CLI to set content type
+        az storage blob update -c "\$web" -n "$filePath" --content-type "application/activity+json;" --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT"
+    done
+' bash "$prefix" {} +
 
-    echo $filePath
-    
-    # Use Azure CLI to set content type
-    az storage blob update -c "\$web" -n "$filePath" --content-type "application/activity+json;" --account-key "$AZURE_STORAGE_KEY" --account-name "$AZURE_STORAGE_ACCOUNT"
-done
-
-#az afd endpoint purge ...
+# you have front door? az afd endpoint purge ...
